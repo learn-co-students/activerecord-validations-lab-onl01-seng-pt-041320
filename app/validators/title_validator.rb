993c98144c3bf40a06845c1fa49140c1e59e6009
OrkunSA <c.orkun.s@gmail.com>
2020-08-23 12:14:01 -0500

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,10 +1,10 @@
 class TitleValidator < ActiveModel::Validator
 
     def validate(record)
-      if record.title
-        unless record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
-        record.errors[:title] << "Please make this more clickbait-y."
+        clickbait_patterns = [/Won't Believe/, /Secret/, /Top [number]/, /Guess/]
+        if clickbait_patterns.none? { |pattern| pattern.match(record.title)}
+            record.errors[:title] << "Please make this more clickbait-y."
         end
-      end
     end
+
 end
