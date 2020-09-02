458c48d1d1809bd58d2e9c247a7d8ac125b8f754
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-01 18:40:52 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
+    validates :content, length: { minimum: 50 }
+    validates :summary, length: { maximum: 50 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
