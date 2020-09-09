c81ee3140d3d3a6a4ff16d9391d7770dccde5430
joeromero47 <joeromero47@gmail.com>
2020-09-09 16:45:08 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  #include ActiveModel::Validations
+  #validates_with PostValidator
+  validates :title, presence: true 
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid size" }
+  validate :is_clickbaitable?
+
+    def is_clickbaitable?
+      @titles = [/Won't Believe/i, /Secret/i, /Top/i, /Guess/i]
+      if @titles.none?{ |click|  click.match title }
+        errors.add(:title, "Must be clickbait!")
+      end
+    end
 end
